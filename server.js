1ffc269db2d8ae0993dcaca9592c0361f1837bc1
Mark <markloercher@gmail.com>
2021-12-14 16:59:58 -0500

    latest

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+function findById (id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 //get function
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -51,6 +56,16 @@ app.get("/api/animals", (req, res) => {
   }
   res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+  
 
 //listen function
 app.listen(PORT, () => {
